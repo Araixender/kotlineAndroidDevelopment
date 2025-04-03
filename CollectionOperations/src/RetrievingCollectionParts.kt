7058e9a0e8e6fe2112b523de719892a4f5aa00cc
Aaraiz Zahid <aaraiz.zahid@gmail.com>
2025-04-03 19:37:17 +0500

    somechanges

--- CollectionOperations/src/RetrievingCollectionParts.kt
+++ CollectionOperations/src/RetrievingCollectionParts.kt
@@ -4,5 +4,14 @@ fun main() {
     println(numbers.slice(1..3))
     println(numbers.slice(0..4 step 2))
     println(numbers.slice(setOf(3, 5, 0)))
+    println()
+    println(numbers.take(3))
+    println(numbers.takeLast(3))
+    println()
+    println(numbers.drop(1))
+    println(numbers.dropLast(5))
+    println()
+    println(numbers.takeWhile { !it.startsWith("F") })
+    println(numbers.takeLastWhile { it != "Three" })
 
 }
